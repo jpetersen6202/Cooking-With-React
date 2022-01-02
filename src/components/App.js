827459e04ab240eac6cc4be7f737b5fcb1438bce
Jake P <jpetersen6202@gmail.com>
2022-01-02 13:07:48 -0600

    finished css for left side of screen. Added classes to jsx.

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ const sampleRecipies = [
     name: 'Plain Chicken',
     servings: 3,
     cookTime: '1:45',
-    instructions: '1. Put salt on chicken\n 2. Put chicken in oven\n 3. Eat chicken',
+    instructions: '1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken',
     ingredients: [
       {
         id: 1,
@@ -33,7 +33,7 @@ const sampleRecipies = [
     name: 'Plain Pork',
     servings: 5,
     cookTime: '0:45',
-    instructions: '1. Put paprika on pork\n 2. Put chicken in oven\n 3. Eat chicken',
+    instructions: '1. Put paprika on pork\n2. Put chicken in oven\n3. Eat chicken',
     ingredients: [
       {
         id: 1,
--- src/components/Recipie.js
+++ src/components/Recipie.js
@@ -6,29 +6,29 @@ export default function Recipie(props) {
 		name, cookTime, servings, instructions, ingredients
 	} = props
 	return (
-		<div>
-			<div>
-				<h3>{name}</h3>
+		<div className='recipie'>
+			<div className='recipie__header'>
+				<h3 className='recipie__title'>{name}</h3>
 				<div>
-					<button>Edit</button>
-					<button>Delete</button>
+					<button className='btn btn--primary mr-1'>Edit</button>
+					<button className='btn btn--danger'>Delete</button>
 				</div>
 			</div>
-			<div>
-				<span>Cook Time:</span>
-				<span>{cookTime}</span>
+			<div className='recipie__row'>
+				<span className='recipie__label'>Cook Time:</span>
+				<span className='recipie__value'>{cookTime}</span>
 			</div>
-			<div>
-				<span>Servings:</span>
-				<span>{servings}</span>
+			<div className='recipie__row'>
+				<span className='recipie__label'>Servings:</span>
+				<span className='recipie__value'>{servings}</span>
 			</div>
-			<div>
-				<span>Instructions:</span>
-				<div>{instructions}</div>
+			<div className='recipie__row'>
+				<span className='recipie__label'>Instructions:</span>
+				<div className='recipie__value recipie__instructions recipie__value--indented'>{instructions}</div>
 			</div>
-			<div>
-				<span>Ingredients:</span>
-				<div>
+			<div className='recipie__row'>
+				<span className='recipie__label'>Ingredients:</span>
+				<div className='recipie__value recipie__value--indented'>
 					<IngredientList ingredients={ingredients} />
 				</div>
 			</div>
